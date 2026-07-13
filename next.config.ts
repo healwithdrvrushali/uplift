1aa02ad9d4843e5374d21124ac0128be8bf4b8ec
healwithdrvrushali <happinesswithdrvrushali@gmail.com>
2026-07-13 14:49:58 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/hls-proxy/:path*",
+        destination: "http://media.drvrushali.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
