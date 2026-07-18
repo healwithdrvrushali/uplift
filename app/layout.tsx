4ca73bfd98a83ea48f9bfeb02e4dfd5b3fdfd5fc
healwithdrvrushali <happinesswithdrvrushali@gmail.com>
2026-07-18 11:03:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Script from "next/script";
+import ClarityScript from "./components/ClarityScript";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -42,17 +43,8 @@ export default function RootLayout({
         </noscript>
         {/* End Meta Pixel Code */}
 
-        {/* Microsoft Clarity */}
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "xo7vc6f94h");
-          `}
-        </Script>
-        {/* End Microsoft Clarity */}
+        {/* Microsoft Clarity - only loads on www.drvrushali.com */}
+        <ClarityScript />
       </head>
       <body className="min-h-screen antialiased">{children}</body>
     </html>
