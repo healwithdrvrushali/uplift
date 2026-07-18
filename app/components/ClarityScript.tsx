89ec521f9fd990b58aa6b8a14e8d3323c269b8a8
healwithdrvrushali <happinesswithdrvrushali@gmail.com>
2026-07-18 11:03:20 +0530

    Add files via upload

---
+++ app/components/ClarityScript.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Script from "next/script";
+import { useEffect, useState } from "react";
+
+export default function ClarityScript() {
+  const [isProduction, setIsProduction] = useState(false);
+
+  useEffect(() => {
+    if (window.location.hostname === "www.drvrushali.com") {
+      setIsProduction(true);
+    }
+  }, []);
+
+  if (!isProduction) return null;
+
+  return (
+    <Script id="microsoft-clarity" strategy="afterInteractive">
+      {`
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "xo7vc6f94h");
+      `}
+    </Script>
+  );
+}
